135c505d2e77117a116932da65fa3de56075b0a1
saintrabby <saintrabby@gmail.com>
2022-04-21 18:41:50 +0900

    fin_fin
    finfin

--- src/shared/apis.js
+++ src/shared/apis.js
@@ -48,8 +48,8 @@ export const apis = {
   postWrite: (fromData) => formDatas.post("/api/post", fromData),
 
   // POST수정 PUT
-  postEdit: (title, category, price, content, postid) =>
-    instance.put(`/api/post/${postid}`, { title, category, price, content }),
+  postEdit: (title, categoryid, price, content, postid) =>
+    instance.put(`/api/post/${postid}`, { title, categoryid, price, content }),
 
   // POST삭제 DELETE
   postDelete: (postid) => instance.delete(`/api/post/${postid}`),
